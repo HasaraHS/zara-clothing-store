af5dd47067bdf9f16b49de04b3bf5448a971ed0c
Hasara Samarawickrama <Hasaragr1920@gmail.com>
2024-07-03 17:47:16 +0530

    commit

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
+import "../globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Zara - Admin Dashboard",
+  description: "Admin dashboard to manage Zara's data",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
